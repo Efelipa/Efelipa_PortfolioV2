7264d394f8f241e2fdf1b6dfe3bb29121579aa5d
Erick Felipa <erickfelipa@gmail.com>
2023-05-12 17:37:40 -0500

    Implement: Main Content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import 'bulma/css/bulma.min.css';
 import './App.css'
 import { NavBar } from './components/Navbar';
+import { Content } from './components/Content';
+
 
 function App() {
   return (
     <div className="App has-navbar-fixed-top">
       <NavBar/>
+      <Content/>
     </div>
   );
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import { Container, Columns, Notification } from 'react-bulma-components';
+
+export const Content = () => {
+    return (
+        <section className='content' id='main'>
+            <Container>
+                <Columns>
+                    
+                </Columns>
+            </Container>
+        </section>
+    )
+}
+
